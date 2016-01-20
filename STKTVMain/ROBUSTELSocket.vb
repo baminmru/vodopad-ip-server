c36575a9846542b651fa97b49bc995e371b859fa
baminmru <bami@nm.ru>
2016-01-20 10:50:36 +0300

    add robustel socket class

---
+++ STKTVMain/ROBUSTELSocket.vb
@@ -0,0 +1,23 @@
+﻿Imports System.Net.Sockets
+
+Public Class ROBUSTELSocket
+    Inherits GRPSSocket
+
+
+    Public Overrides ReadOnly Property SocketType() As String
+        Get
+            Return "ROBUSTELSOCKET"
+        End Get
+    End Property
+
+
+    Public Sub New(ByRef aSocket As Socket)
+        MyBase.New(aSocket)
+    End Sub
+
+
+
+
+
+
+End Class
